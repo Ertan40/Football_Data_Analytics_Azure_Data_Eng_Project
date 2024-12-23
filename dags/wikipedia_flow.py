1d7b07fb668e68767c0242ceb0f67f9640c7feda
Ertan Donmez <ertandonmez1@gmail.com>
2024-12-23 19:18:45 +0200

    refactor

---
+++ dags/wikipedia_flow.py
@@ -0,0 +1,56 @@
+from airflow import DAG
+from datetime import datetime
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.insert(0, '/opt/airflow/pipelines')
+
+from airflow.operators.python import PythonOperator
+from pipelines.wikipedia_pipeline import extract_wikipedia_data, transform_wikipedia_data, write_wikipedia_data
+
+sys.path.insert(0, '/opt/airflow/pipelines')
+dag = DAG(
+    dag_id='wikipedia_flow',
+    default_args={
+        "owner": "Ertan Donmez",
+        "start_date": datetime(2024, 12, 15),
+    },
+    schedule_interval=None,
+    catchup=False
+)
+# Extraction
+extract_data_from_wikipedia = PythonOperator(
+    task_id="extract_data_from_wikipedia",
+    python_callable=extract_wikipedia_data,
+    provide_context=True,
+    op_kwargs={
+        "url": "https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_capacity"
+    },
+    dag=dag
+)
+
+# Preprocessing
+transform_wikipedia_data = PythonOperator(
+    task_id="transform_wikipedia_data",
+    python_callable=transform_wikipedia_data,
+    provide_context=True,
+    dag=dag
+)
+
+# Write
+write_wikipedia_data = PythonOperator(
+    task_id="write_wikipedia_data",
+    python_callable=write_wikipedia_data,
+    provide_context=True,
+    dag=dag
+)
+
+extract_data_from_wikipedia >> transform_wikipedia_data >> write_wikipedia_data
+
+
+
+
+
+
